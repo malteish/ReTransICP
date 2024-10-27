d2a0bbfa4a351c67b5e802930883a3657807d124
malteish <malteish@corpus.io>
2024-10-27 11:48:54 +0100

    add impressum

--- webapp/src/components/Info.tsx
+++ webapp/src/components/Info.tsx
@@ -3,7 +3,7 @@ import { GITHUB_LICENSE, GITHUB_SOURCE_CODE } from "../utils/constants";
 export function Info() {
   return (
     <div className="steps-container heading-text info">
-      DISCLAIMER: This tool is provided as is under the{" "}
+      DISCLAIMER: This DEMO tool is provided as is under the{" "}
       <a className="link" href={GITHUB_LICENSE}>
         BSD 2-Clause License
       </a>
@@ -11,7 +11,7 @@ export function Info() {
       and do not use it for production. You can find the source code, open
       issues and contribute{" "}
       <a className="link" href={GITHUB_SOURCE_CODE}>
-        here.
+        in this github repository.
       </a>{" "}
       <br />
       The favicon was was derived from{" "}
@@ -22,6 +22,15 @@ export function Info() {
         a work by Emoji One
       </a>
       .
+      <div className="impressum">
+        <h2>Impressum</h2>
+        <p>
+          This DEMO is provided by malteish. Reach out to us through{" "}
+          <a className="link" href="mailto:ReTransICP@malteish.com">
+            ReTransICP@malteish.com
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
